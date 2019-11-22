0da8a540d18e18ba092c974271d282d21223a71a
Chase <joli.chase@gmail.com>
2019-11-21 19:33:50 -0700

    Can register and login, get confirmation when logging in successfully

--- client/main.go
+++ client/main.go
@@ -8,6 +8,7 @@ import (
 	prompt "github.com/c-bata/go-prompt"
 	"github.com/hibooboo2/gchat/api"
 	"github.com/hibooboo2/gchat/utils"
+	"github.com/rivo/tview"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
@@ -19,26 +20,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("can not connect with server %v", err)
 	}
+
 	// create stream
 	chatClient := api.NewChatClient(conn)
 	authClient := api.NewAuthClient(conn)
 	friendsClient := api.NewFriendsClient(conn)
 
 	es := &ExecutorScope{authClient: authClient, chatClient: chatClient, friendClient: friendsClient}
-	p := prompt.New(es.executor, Commands)
-
-	p.Run()
-
-	ctx := context.Background()
-	// The following code is how you add metadata to the context for the server to use it.
-	ctx = metadata.AppendToOutgoingContext(ctx, "TOKEN", "some token from auth")
-
-	statusClient, err := friendsClient.Status(ctx, nil)
-	if err != nil {
-		panic(err)
-	}
-	statusClient.Recv()
-
+	es.ui()
 }
 
 type ExecutorScope struct {
@@ -46,6 +35,8 @@ type ExecutorScope struct {
 	ctx          context.Context
 	chatClient   api.ChatClient
 	friendClient api.FriendsClient
+	friendList   map[string]*api.Friend
+	app          *tview.Application
 }
 
 func (e *ExecutorScope) executor(t string) {
@@ -54,9 +45,6 @@ func (e *ExecutorScope) executor(t string) {
 	switch t {
 	case "register":
 		err = reg(e.authClient)
-	case "login":
-		e.ctx, err = login(e.authClient)
-
 	case "message":
 		e.sendMessage()
 	case "notifications":
@@ -65,6 +53,10 @@ func (e *ExecutorScope) executor(t string) {
 		e.sendFriendRequest()
 	case "friends list":
 		e.getFriends()
+	case "remove friend":
+		e.removeFriend()
+	case "status":
+		e.status()
 	}
 	if err != nil {
 		fmt.Println(err)
@@ -98,27 +90,29 @@ func Commands(d prompt.Document) []prompt.Suggest {
 		{Text: "notifications", Description: "Pull up notifications"},
 		{Text: "send friend request", Description: "Send a user a friend request"},
 		{Text: "friends list", Description: "Get a list of your friends"},
+		{Text: "remove friend", Description: "Removes a friend from your friends list"},
+		{Text: "status", Description: "checks the status of friends  from your friends list"},
 	}
+
 	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
 
-func login(authClient api.AuthClient) (context.Context, error) {
-
+func (e *ExecutorScope) login(username string, pass string) error {
 	in := api.LoginRequest{
-		Username: prompt.Input("What is your username? ", Empty),
-		Password: prompt.Input("What is your password? ", Empty),
+		Username: username,
+		Password: pass,
 	}
 
 	in.Password = utils.Hash(in.Password)
 
 	ctx := context.Background()
-	l, err := authClient.Login(ctx, &in)
+	l, err := e.authClient.Login(ctx, &in)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	ctx = metadata.AppendToOutgoingContext(ctx, "TOKEN", l.Token)
+	e.ctx = metadata.AppendToOutgoingContext(ctx, "TOKEN", l.Token)
 
-	return ctx, nil
+	return nil
 }
 
 func (e *ExecutorScope) sendMessage() {
@@ -160,12 +154,141 @@ func (e *ExecutorScope) sendFriendRequest() {
 }
 
 func (e *ExecutorScope) getFriends() {
+	e.friendList = map[string]*api.Friend{}
 	friends, err := e.friendClient.All(e.ctx, &api.FriendsListReq{})
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, friendsList := range friends.Friends {
-		fmt.Println(friendsList)
+	for _, friend := range friends.Friends {
+		e.friendList[friend.Username] = friend
 	}
+
+}
+
+func (e *ExecutorScope) removeFriend() {
+	username := prompt.Input("Which friend do you want to delete?", Empty)
+	_, err := e.friendClient.Remove(e.ctx, &api.Friend{
+		Username: username,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	delete(e.friendList, username)
+
+}
+
+func (e *ExecutorScope) status() {
+	stream, err := e.friendClient.Status(e.ctx, &api.Empty{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	go func() {
+		for {
+			status, err := stream.Recv()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			friend, ok := e.friendList[status.Username]
+			if ok {
+				friend.Status = status.Status
+			}
+		}
+	}()
+}
+
+func (e *ExecutorScope) ui() {
+	e.app = tview.NewApplication()
+	list := tview.NewList()
+	list.AddItem("register", "register to use the program", 'r', func() { e.registerScreen(list) })
+	list.AddItem("login", "login as a user", 'l', func() { e.loginScreen(list) })
+	list.AddItem("quit", "quit the program", 'q', func() { e.app.Stop() })
+
+	if err := e.app.SetRoot(list, true).SetFocus(list).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (e *ExecutorScope) registerScreen(elementToFocus tview.Primitive) {
+	req := api.RegisterRequest{}
+	form := tview.NewForm()
+	form.AddInputField("Email", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+		req.Email = text
+	})
+
+	form.AddInputField("Username", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+		req.Username = text
+	})
+	form.AddPasswordField("Password", "", 10, '*', func(text string) {
+		req.Password = text
+	})
+	form.AddInputField("First Name", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+		req.FirstName = text
+	})
+	form.AddInputField("Last Name", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+		req.LastName = text
+	})
+
+	form.AddButton("Register", func() {
+		_, err := e.authClient.Register(context.Background(), &req)
+		if err != nil {
+			e.modal(form, err.Error())
+			return
+		}
+		e.app.SetRoot(elementToFocus, true).SetFocus(elementToFocus).Draw()
+	})
+	e.app.SetRoot(form, true).SetFocus(form).Draw()
+
+}
+func (e *ExecutorScope) loginScreen(elementToFocus tview.Primitive) {
+	form := tview.NewForm()
+	username := ""
+	form.AddInputField("Username", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+		username = text
+	})
+	pass := ""
+	form.AddPasswordField("Password", "", 10, '*', func(text string) {
+		pass = text
+	})
+
+	form.AddButton("Login", func() {
+		err := e.login(username, pass)
+		if err != nil {
+			e.modal(form, err.Error())
+			return
+		}
+		e.modal(elementToFocus, "Logged in as "+username)
+	})
+
+	e.app.SetRoot(form, true).SetFocus(form).Draw()
+}
+
+//func (e *ExecutorScope) messageScreen(elementToFocus tview.Primitive) {
+//	form := tview.NewForm()
+//	message := ""
+//	username := ""
+//	form.AddInputField("Username", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+//
+//	})
+//
+//	form.AddButton("send message", func() {
+//		_, err := e.messageScreen(elementToFocus)
+//		if err != nil {
+//			e.errorModal(elementToFocus)
+//		}
+//		e.app.SetRoot(elementToFocus, true).SetFocus(elementToFocus).Draw()
+//	})
+//	form.AddInputField("Message", "", 10, tview.InputFieldMaxLength(100), func(text string) {
+//
+//	})
+//}
+func (e *ExecutorScope) modal(elementToFocus tview.Primitive, s string) {
+	m := tview.NewModal()
+	m.AddButtons([]string{"Ok"}).SetText(s)
+	m.SetDoneFunc(func(num int, s string) {
+		e.app.SetRoot(elementToFocus, true).SetFocus(elementToFocus).Draw()
+	})
+	e.app.SetRoot(m, true).SetFocus(m).Draw()
 
 }
